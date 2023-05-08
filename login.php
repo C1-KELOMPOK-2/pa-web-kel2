a98074ee2e3947d11dc03437a5c7b6f92f57f938
harist13 <razak1352.com@gmail.com>
2023-05-09 00:06:33 +0800

    menambahkan fungsi php pada login dan register

--- login.php
+++ login.php
@@ -1,3 +1,61 @@
+<?php
+session_start();
+require "koneksi.php";
+
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+
+    $query_admin = "SELECT * FROM petugas WHERE username='$username' AND password='$password' AND role='admin'";
+    $query_petugas = "SELECT * FROM petugas WHERE username='$username' AND password='$password' AND role='petugas'";
+
+    $query_masyarakat = "SELECT * FROM masyarakat WHERE username='$username' AND password='$password' AND role='masyarakat'";
+
+    $result_admin = mysqli_query($conn, $query_admin);
+    $result_petugas = mysqli_query($conn, $query_petugas);
+
+    $result_masyarakat = mysqli_query($conn, $query_masyarakat);
+
+    if (mysqli_num_rows($result_admin) == 1) {
+        $_SESSION['role'] = 'admin';
+        $_SESSION['username'] = $username;
+        $row = mysqli_fetch_assoc($result_admin);
+        $_SESSION['nama_petugas'] = $row['nama_petugas'];
+        $_SESSION['id_petugas'] = $row['id_petugas'];
+
+        header("location: admin/index.php");
+	}else if (mysqli_num_rows($result_petugas) == 1) {
+			$_SESSION['role'] = 'petugas';
+			$_SESSION['username'] = $username;
+			$row = mysqli_fetch_assoc($result_petugas);
+			$_SESSION['id_petugas'] = $row['id_petugas'];
+			$_SESSION['nama_petugas'] = $row['nama_petugas'];
+			header("location: petugas/index.php");
+    } else if (mysqli_num_rows($result_masyarakat) == 1) {
+        $_SESSION['role'] = 'masyarakat';
+        $_SESSION['username'] = $username;
+        $row = mysqli_fetch_assoc($result_masyarakat);
+        $_SESSION['nama'] = $row['nama'];
+        $_SESSION['nik'] = $row['nik'];
+
+        header("location: masyarakat/index.php");
+    } else {
+        echo "<script>
+            alert('Username dan password salah.');
+        </script>";
+    }
+}
+mysqli_close($conn);
+?>
+
+
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -29,22 +87,22 @@
                     </div>
 
                     <div class="signin-form">
-                        <h2 class="form-title">Sign in</h2>
+                        <h2 class="form-title">Login</h2>
                         <form method="POST" class="register-form" id="login-form">
                             <div class="form-group">
                                 <label for="your_name"><i class="zmdi zmdi-account material-icons-name"></i></label>
-                                <input type="text" name="your_name" id="your_name" placeholder="Your Name"/>
+                                <input type="text" name="username" id="your_name" placeholder="Your Name"/>
                             </div>
                             <div class="form-group">
                                 <label for="your_pass"><i class="zmdi zmdi-lock"></i></label>
-                                <input type="password" name="your_pass" id="your_pass" placeholder="Password"/>
+                                <input type="password" name="password" id="your_pass" placeholder="Password"/>
                             </div>
                             <div class="form-group">
                                 <input type="checkbox" name="remember-me" id="remember-me" class="agree-term" />
                                 <label for="remember-me" class="label-agree-term"><span><span></span></span>Remember me</label>
                             </div>
                             <div class="form-group form-button">
-                                <input type="submit" name="signin" id="signin" class="form-submit" value="Log in"/>
+                                <input type="submit"  id="signin" class="form-submit" value="Log in"/>
                             </div>
                         </form>
                        
--- register.php
+++ register.php
@@ -1,3 +1,84 @@
+<?php
+require "koneksi.php";
+
+if (isset($_POST['submit'])) {
+    // Get input data
+    $nama = $_POST['nama'];
+    $nik = $_POST['nik'];
+    $username = $_POST['username'];
+    $password = $_POST['password1'];
+    $telp = $_POST['telp'];
+    $role = $_POST['role'];
+    
+    // Check if the gambar key is set in the $_FILES array
+    if (isset($_FILES['gambar'])) {
+        $gambar = $_FILES['gambar']['name'];
+        
+        // Check if username already exists
+        $check_username = mysqli_query($conn, "SELECT * FROM masyarakat WHERE username='$username'");
+        $check_nik = mysqli_query($conn, "SELECT * FROM masyarakat WHERE nik='$nik'");
+
+        if (mysqli_num_rows($check_username) > 0) {
+            echo "<script>alert('Username sudah digunakan');</script>";
+        }else if (mysqli_num_rows($check_nik) > 0) {
+                echo "<script>alert('nik sudah digunakan');</script>";
+        
+        } else {
+            // Insert data into database
+            $insert = mysqli_query($conn, "INSERT INTO masyarakat (nama, nik, username, password, telp, role, gambar) VALUES ('$nama', '$nik', '$username', '$password', '$telp', '$role', '$gambar')");
+            if ($insert) {
+                // Upload image to server
+                $target_dir = "uploads/";
+                $target_file = $target_dir . basename($_FILES["gambar"]["name"]);
+                move_uploaded_file($_FILES["gambar"]["tmp_name"], $target_file);
+
+                echo "<script>
+                alert('Berhasil register');
+                document.location.href = 'login.php';
+                </script>";
+            } else {
+                echo "<script>alert('Gagal mendaftar');</script>";
+            }
+        }
+    } else {
+        // If the gambar key is not set, set the $gambar variable to an empty string
+        $gambar = "";
+        
+        // Check if username already exists
+        $check_username = mysqli_query($conn, "SELECT * FROM masyarakat WHERE username='$username'");
+        $check_nik = mysqli_query($conn, "SELECT * FROM masyarakat WHERE nik='$nik'");
+
+        if (mysqli_num_rows($check_username) > 0) {
+            echo "<script>alert('Username sudah digunakan');</script>";
+
+        }else if (mysqli_num_rows($check_nik) > 0) {
+            echo "<script>alert('nik sudah digunakan');</script>";
+    
+        } else {
+            // Insert data into database
+            $insert = mysqli_query($conn, "INSERT INTO masyarakat (nama, nik, username, password, telp, role, gambar) VALUES ('$nama', '$nik', '$username', '$password', '$telp', '$role', '$gambar')");
+            if ($insert) {
+                echo "<script>
+                alert('Berhasil register');
+                document.location.href = 'login.php';
+                </script>";
+            } else {
+                echo "<script>alert('Gagal mendaftar');</script>";
+            }
+        }
+    }
+}
+?>
+
+
+
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -21,30 +102,45 @@
             <div class="container">
                 <div class="signup-content">
                     <div class="signup-form">
-                        <h2 class="form-title">Sign up</h2>
-                        <form method="POST" class="register-form" id="register-form">
+                        <h2 class="form-title">Register</h2>
+                        <form method="POST" class="register-form" id="register-form" enctype="multipart/form-data">
                             <div class="form-group">
                                 <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
-                                <input type="text" name="name" id="name" placeholder="Your Name"/>
+                                <input type="text" name="nama" id="name" placeholder="namamuuu"/>
+                            </div>
+                            <div class="form-group">
+                                <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                                <input type="text" name="nik" id="name" placeholder="nik"/>
                             </div>
                             <div class="form-group">
                                 <label for="email"><i class="zmdi zmdi-email"></i></label>
-                                <input type="email" name="email" id="email" placeholder="Your Email"/>
+                                <input type="text" name="username" id="email" placeholder="username"/>
                             </div>
                             <div class="form-group">
                                 <label for="pass"><i class="zmdi zmdi-lock"></i></label>
-                                <input type="password" name="pass" id="pass" placeholder="Password"/>
+                                <input type="password" name="password1" id="pass" placeholder="Password"/>
                             </div>
                             <div class="form-group">
                                 <label for="re-pass"><i class="zmdi zmdi-lock-outline"></i></label>
-                                <input type="password" name="re_pass" id="re_pass" placeholder="Repeat your password"/>
+                                <input type="password" name="password2" id="re_pass" placeholder="Repeat your password"/>
+                            </div>
+                            <div class="form-group">
+                                <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                                <input type="text" name="telp" id="name" placeholder="telponmuuuu"/>
+                            </div>
+                            <div class="form-group">
+                                <span>masukan foto/ktp</span>
+                               
+                                <input required="required" type="file" name="gambar" class="form-control-file" id="gambar" aria-describedby="gambarHelp" accept="image/*">
                             </div>
                             <div class="form-group">
                                 <input type="checkbox" name="agree-term" id="agree-term" class="agree-term" />
                                 <label for="agree-term" class="label-agree-term"><span><span></span></span>I agree all statements in  <a href="#" class="term-service">Terms of service</a></label>
                             </div>
+                                <input type="hidden" name="role" id="name" placeholder="telponmuuuu"/>
+                           
                             <div class="form-group form-button">
-                                <input type="submit" name="signup" id="signup" class="form-submit" value="Register"/>
+                                <input type="submit" name="submit" id="signup" class="form-submit" value="Register"/>
                             </div>
                         </form>
                     </div>

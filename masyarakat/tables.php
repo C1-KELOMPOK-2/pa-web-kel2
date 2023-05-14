5f9c65153f7f8676ec9c740d00b4fc447ef78365
harist13 <razak1352.com@gmail.com>
2023-05-14 22:06:29 +0800

    menambahkan alert dan menghapus tampilan id tanggapan pada di tanggapi

--- masyarakat/tables.php
+++ masyarakat/tables.php
@@ -229,7 +229,7 @@ if(isset($_GET['id_pengaduan'])){?>
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Tanggapan <?= "[ID: ".$keluartgp['id_tanggapan']."]";?></h5>
+        <h5 class="modal-title" id="exampleModalLabel">Tanggapan</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
